1a5948bcb2b5cd15ffed585d627db28edb5394e7
freville.franck@gmail.com <freville.franck@gmail.com>
2023-07-28 11:59:00 +0200

    nav-bar

---
+++ test/components/nav.php
@@ -0,0 +1,26 @@
+<header>
+    <nav>
+
+        <a href="/" id="logo">TOTO</a>
+        <i class="fa-solid fa-bars" id="ham-menu"></i>
+        <ul id="nav-bar">
+            <li>
+                <a href="/">Accueil</a>
+            </li>
+            <li>
+                <a href="./pages/a_propos.php">Compétences</a>
+            </li>
+            <li>
+                <a href="./pages/a_propos.php">Projets</a>
+            </li>
+            <li>
+                <a href="./pages/a_propos.php">A propos</a>
+            </li>
+            <li>
+                <a href="./pages/a_propos.php">Contact</a>
+            </li>
+        </ul>
+
+    </nav>
+
+</header>
